6a49d82a6af383b70338d1eded22f3658e5d69d3
Gemma Mason <gemmaellen@gmail.com>
2019-08-21 16:41:33 +1200

    Swap two lines

--- spectral/tests/spymath.py
+++ spectral/tests/spymath.py
@@ -119,8 +119,8 @@ class PCATest(SpyTest):
         assert(np.allclose(evecs.T.dot(evecs), np.eye(evecs.shape[0])))
 
     def test_consistency_with_stats(self):
-        import spectral as spy
         '''Check principal_components(stats) = principal_components(data).'''
+        import spectral as spy
         pc_from_stats = spy.principal_components(self.pc.stats)
         assert(np.allclose(self.pc.stats.cov, pc_from_stats.stats.cov))
         assert(np.allclose(self.pc.stats.mean, pc_from_stats.stats.mean))
